4b59964a54cfddce67eb8868e914b535457090f4
Mawa-E241426 <157117518+Kazijannatulmawa@users.noreply.github.com>
2025-07-06 19:40:24 +0600

    Update A.cpp

---
+++ Contest 2/A.cpp
@@ -0,0 +1,20 @@
+//Problem Name   : Timon and Pumbaa
+//Teacher’s Name : Mirza Raquib
+//Course Title   : Competitive programming sessional 
+//Course Code    : CCE-2310
+//Date           : 19.05.25
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  long long a,b;
+  cin>>a>>b;
+  if(a>b)
+  {
+    cout<<a-b<<endl;
+  }
+  else
+  {
+    cout<<0<<endl;
+  }
+}
