453fc1f1e09304590d143c0766019558a41f20b8
Mawa-E241426 <157117518+Kazijannatulmawa@users.noreply.github.com>
2025-07-06 20:47:39 +0600

    Update F.cpp

--- Data Type/F.cpp
+++ Data Type/F.cpp
@@ -1,3 +1,8 @@
+//Problem Name   : Digits Summation
+//Teacher’s Name : Mirza Raquib
+//Course Title   : Competitive programming sessional
+//Course Code    : CCE-2310
+//Date           : 24.02.25
 #include<bits/stdc++.h>
 using namespace std;
 int main()
