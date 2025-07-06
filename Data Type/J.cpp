f867ad07d5c8901bc49dd4395ae2de2c6f2688d6
Mawa-E241426 <157117518+Kazijannatulmawa@users.noreply.github.com>
2025-07-06 20:57:08 +0600

    Update J.cpp

--- Data Type/J.cpp
+++ Data Type/J.cpp
@@ -1,6 +1,10 @@
+//Problem Name   : Multipuls
+//Teacher’s Name : Mirza Raquib
+//Course Title   : Competitive programming sessional
+//Course Code    : CCE-2310
+//Date           : 24.02.25
 #include <iostream>
 using namespace std;
-
 int main()
 {
   long long a,b;
