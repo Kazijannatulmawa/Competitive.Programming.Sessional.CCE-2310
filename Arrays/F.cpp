77687386d22e17ccf0d9e8336e3ae4288a1795ca
Mawa-E241426 <157117518+Kazijannatulmawa@users.noreply.github.com>
2025-07-06 19:49:28 +0600

    Update F.cpp

--- Arrays/F.cpp
+++ Arrays/F.cpp
@@ -1,3 +1,8 @@
+//Problem Name   : Reversing 
+//Teacher’s Name : Mirza Raquib
+//Course Title   : Competitive programming sessional 
+//Course Code    : CCE-2310
+//Date           : 26.05.25
 #include <bits/stdc++.h>
 using namespace std;
 int main()
