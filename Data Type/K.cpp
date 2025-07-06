a6915cfddb2091e42ee25dcb5a799e8e2d73a1fa
Mawa-E241426 <157117518+Kazijannatulmawa@users.noreply.github.com>
2025-07-06 21:01:00 +0600

    Update K.cpp

--- Data Type/K.cpp
+++ Data Type/K.cpp
@@ -1,3 +1,8 @@
+//Problem Name   : Max and Min
+//Teacher’s Name : Mirza Raquib
+//Course Title   : Competitive programming sessional
+//Course Code    : CCE-2310
+//Date           : 04.05.25
 #include <iostream>
 using namespace std;
 int main()
