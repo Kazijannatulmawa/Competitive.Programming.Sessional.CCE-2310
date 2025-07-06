b3cde0ac0b2b6424eaa58bdc24e1c111d9cb7268
Mawa-E241426 <157117518+Kazijannatulmawa@users.noreply.github.com>
2025-07-06 19:46:19 +0600

    Update C.cpp

--- Arrays/C.cpp
+++ Arrays/C.cpp
@@ -1,3 +1,8 @@
+//Problem Name   : Replacement 
+//Teacher’s Name : Mirza Raquib
+//Course Title   : Competitive programming sessional 
+//Course Code    : CCE-2310
+//Date           : 26.05.25
 #include <bits/stdc++.h>
 using namespace std;
 int main()
